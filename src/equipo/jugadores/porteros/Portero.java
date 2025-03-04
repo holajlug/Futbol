93b010389fd1750a4376ca4ff47f6832a098e056
mariaJimenez <mjimgon@g.educaand.es>
2025-03-04 10:52:05 +0100

    portero

--- src/equipo/jugadores/porteros/Portero.java
+++ src/equipo/jugadores/porteros/Portero.java
@@ -1,5 +1,37 @@
 package equipo.jugadores.porteros;
 
-public class Portero {
+import equipo.jugadores.Jugador;
 
+public class Portero extends Jugador{
+	
+	private int paradas;
+	
+	public Portero(String nombre, int dorsal, String equipo) {
+		super(nombre, dorsal, equipo);
+		setParadas(paradas);
+	}
+
+	public int getParadas() {
+		return paradas;
+	}
+
+	public void setParadas(int paradas) {
+		this.paradas = paradas;
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	@Override 
+	public boolean equals(Object obj) {
+		Portero port = (Portero) obj;
+		boolean resultado = false;
+		if(port.getId_Jug() == this.getId_Jug()){
+			resultado = true;
+		}
+		return resultado;
+	}
+	
 }
